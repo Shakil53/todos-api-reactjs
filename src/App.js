8e21585792a00a52747c68054079126a419f10f1
Shakil53 <shakilahmed.dcc@gmail.com>
2022-12-03 00:26:25 +0600

    done project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList></TodoList>
+      
     </div>
   );
 }
+
+function TodoList() {
+
+  const [todos, setTodos] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(res => res.json())
+      .then(data => setTodos(data))
+  }, [])
+  
+      
+  return (
+    <div>
+      <h1>Todos Items:-{todos.length}</h1>
+      <div style={{display: 'grid',gridTemplateColumns: 'repeat(3, 1fr)' }} >
+        
+      {
+          todos.map(todo => <Post title={todo.title} completed={todo.completed} id={todo.id} key={todo.id}></Post>
+          )
+        }  
+        key= "userId";
+      </div>
+      
+    </div>
+  )
+}
+
+function Post(props) {
+  return (
+    <div style={{ backgroundColor: '#DEBACE', color: '#2B3A55', padding: '8px', border: '2px solid #6D67E4', margin: '10px', borderRadius: '20px'}}>
+      
+      <h3>Title:- {props.title}</h3>
+      <p>Todo Items num: {props.id}</p>
+      <p>Completed Status:{props.true}</p>
+    </div>
+  )
+}
+
 
 export default App;
